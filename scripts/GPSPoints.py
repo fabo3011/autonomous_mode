d4b92262b0136167c73bebef2970f4ceec6b3cac
Fabian Gomez <a01209914@itesm.mx>
2019-02-27 14:33:25 -0600

    Add /ontarget publisher - Debug GPSListOfPoints class

--- scripts/GPSPoints.py
+++ scripts/GPSPoints.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
+#node by fgg for EagleX
+#Fabian Gomez Gonzalez
+#a01209914@itesm.mx
+
+#Project ALVIN (Autonomous Logic Virtual Intelligence n' Navigation)
+
 import rospy
 import roslib
 import os
 from gps_common.msg import GPSFix
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 from lib.GPSVector import *
 
 #temp constants
-minDistanceToFinalTarget = 5.00
+minDistanceToFinalTarget = 2.00 # 2m
+minDistanceToOtherTarget = 2.00 # 2m
 
 #path where the list of GPS points [latitude, longitude] is located
-directory = os.path.expanduser("~/catkin_ws/src/autonomous_mode/GPS_files/")
+#directory = os.path.expanduser("~/catkin_ws/src/autonomous_mode/GPS_files/")
 filename = "log.txt"
 
 gpsData   = GPSFix()
@@ -31,22 +39,6 @@ def callback(data):
     gpsData_point = GPSPoint(gpsData.latitude, gpsData.longitude)
     
 #Logic Functions
-
-#Load Points From File
-#This funciton loads the list of points (pairs of GPS coordinates [latitude, longitude]) contained in pathToFile published by the GenerateList.py node
-def loadPointsFromFile(pathToFile):
-    points = []
-    #open file with GPS coordinate points
-    FILE = open(pathToFile,"r")
-    #read first line
-    line = FILE.readline()
-    while len(line) > 0:    #while there exists a next line to read
-        points.append(line.split())
-        line = FILE.readline()
-    #close file
-    FILE.close()
-    #return list of points
-    return points
 
 #function to test if the rover has arrived to the target (within minDistanceToFinalTarget range)
 def arrivedToFinalTarget(data, target):
@@ -79,7 +71,7 @@ def closerToNextTarget(data, target, next_target):
     next_to_target = GPSVector(next_target, target)
     
     #Compare distances and checks if data is closer to next target
-    if next_to_data.magnitude <= next_to_target.magnitude:
+    if next_to_data.magnitude <= next_to_target.magnitude or dest_vec.magnitude <= minDistanceToOtherTarget:
         print('Target Reached!')
         print('SUCCESS')
         return True
@@ -98,13 +90,15 @@ def GpsPoints():
     global gpsData_point
     global gpsTarget_point
     
-    pub = rospy.Publisher('gpstarget', GPSFix, queue_size=10)
+    pub        = rospy.Publisher('gpstarget', GPSFix, queue_size=10)
+    pub_target = rospy.Publisher('onfinaltarget', String, queue_size=10)
     rospy.init_node('GPSPoints', anonymous=True)
-    rospy.Subscriber('GPS',GPSFix,callback)
+    rospy.Subscriber('gpssim',GPSFix,callback)
     rospy.on_shutdown(bye)
 
     #obtain list of points from file
-    points = loadPointsFromFile(directory+filename)
+    points = GPSListOfPoints()
+    points.loadListFromFile(filename)
     
     #index that points to the current point to publish in /gpstarget
     curr_point_idx = 0
@@ -115,8 +109,8 @@ def GpsPoints():
         
         #get [lat, lon] from current point
         if curr_point_idx != len(points):
-            gpsTarget.latitude  = float(points[curr_point_idx][0])
-            gpsTarget.longitude = float(points[curr_point_idx][1])
+            gpsTarget.latitude  = points[curr_point_idx].latitude
+            gpsTarget.longitude = points[curr_point_idx].longitude
         #Get GPSPoint for GPSTarget
         gpsTarget_point = GPSPoint(gpsTarget.latitude, gpsTarget.longitude)
         #publish gpsTarget
@@ -126,18 +120,21 @@ def GpsPoints():
         
         if curr_point_idx == len(points)-1:     #target is the last one
             
+            pub_target.publish('loading')
             #function to evaluate if its close enough to arrive to the final target
             if arrivedToFinalTarget(gpsData_point, gpsTarget_point):
                 gpsTarget.latitude  = 0.0
                 gpsTarget.longitude = 0.0
                 pub.publish(gpsTarget)
                 curr_point_idx += 1
+                pub_target.publish('done')
                 
         elif curr_point_idx == len(points):     #final target has been reached
+            exit(0)
             pass    
         else:
             #check if its closer to the next target in the list
-            next_target_point = GPSPoint(float(points[curr_point_idx+1][0]), float(points[curr_point_idx+1][1]))
+            next_target_point = GPSPoint(points[curr_point_idx+1])
             if closerToNextTarget(gpsData_point, gpsTarget_point, next_target_point):
                 curr_point_idx += 1
             
